039471d97856e923bfb1c2fe08806de3d93a8e80
hoangmanht <palavipa@gmail.com>
2024-08-27 16:07:48 +0700

    commit

--- lib/Widgets/screens/home_screen/my_home_screen.dart
+++ lib/Widgets/screens/home_screen/my_home_screen.dart
@@ -1,18 +1,88 @@
+import 'package:carousel_indicator/carousel_indicator.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:lazada_clone/Widgets/screens/home_screen/top_bar.dart';
+import 'package:lazada_clone/utility/banner_list.dart';
 import 'package:lazada_clone/utility/colors.dart';
+import 'package:lazada_clone/utility/screen_list.dart';
 
-class MyHomeScreen extends StatelessWidget {
+class MyHomeScreen extends StatefulWidget {
   MyHomeScreen({super.key});
 
+  @override
+  State<MyHomeScreen> createState() => _MyHomeScreenState();
+}
+
+class _MyHomeScreenState extends State<MyHomeScreen> {
   // bottom navigation index
   int currentIndex = 0;
+
+  // carousel index
+  int carouselIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: MyColors.background,
-      body: const TopBar(),
+      body: Column(
+        children: [
+          // top bar
+          const TopBar(),
+
+          // carousel slider
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 15),
+            height: 300,
+            color: MyColors.white,
+            child: Column(
+              children: [
+                Stack(
+                  children: [
+                    CarouselSlider(
+                      options: CarouselOptions(
+                        autoPlay: true,
+                        viewportFraction: 1.0,
+                        height: 170,
+                        onPageChanged: (index, reason) {
+                          setState(() {
+                            carouselIndex = index;
+                          });
+                        },
+                      ),
+                      items: BannerList.imageUrls.map((url) {
+                        return Builder(builder: (BuildContext context) {
+                          return Container(
+                            width: double.infinity,
+                            margin: const EdgeInsets.all(10),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(12),
+                              child: Image.network(
+                                url,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          );
+                        });
+                      }).toList(),
+                    ),
+                    Positioned(
+                      bottom: 20,
+                      left: 150,
+                      child: CarouselIndicator(
+                        cornerRadius: 100,
+                        height: 10,
+                        width: 10,
+                        index: carouselIndex,
+                        count: ScreenList.myScreen.length,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/utility/banner_list.dart
@@ -0,0 +1,9 @@
+class BannerList {
+  static final List<String> imageUrls = [
+    'https://img.freepik.com/free-vector/mega-sale-offers-banner-template_1017-31299.jpg?w=1380&t=st=1724730121~exp=1724730721~hmac=897b236fc319b5ff7ffe7fa7a0f5b4513ea950c35c27b4d95dcce0095db9725c',
+    'https://img.freepik.com/free-vector/super-sale-with-best-price-background_23-2148960122.jpg?w=1380&t=st=1724730167~exp=1724730767~hmac=e1c3724a3a3ea1fd4d83a02c4102624e7cfaacb8c4f555a60a1d8c0e7b03d2ac',
+    'https://img.freepik.com/free-vector/super-sale-with-special-price-background_23-2148960119.jpg?w=1380&t=st=1724730259~exp=1724730859~hmac=3042fb7a55f09f8f0bff8ccf3caf966ae37febd9dee255c99c62ff7a2333d6bf',
+    'https://img.freepik.com/free-vector/colorful-3d-sales-background-concept_52683-32616.jpg?w=1380&t=st=1724730346~exp=1724730946~hmac=dd3731c58d31233d904783c190b87d3b656635462f86ba1b0dfb2829714326c8',
+    'https://img.freepik.com/free-vector/big-sale-offer-template-with-discount-coupon-3d-style_1017-39110.jpg?w=1380&t=st=1724730632~exp=1724731232~hmac=58add3e2359704d838a8855b1833009eadd14e35e8fbaafb65cda2dacf415c3c'
+  ];
+}
